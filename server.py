33bd56b696a97308eb2dd3434e3217dfb8f4c2c0
kyewei <kyewei22@yahoo.ca>
2014-12-24 18:48:39 -0500

    IT WORKS AHH

--- server.py
+++ server.py
@@ -1,8 +1,59 @@
 #!/usr/bin/env python2.7
+
+#Import Twisted Network Library
 from twisted.internet.protocol import Factory, Protocol
 from twisted.internet import reactor
 
 
+#Distinguish between OS
+#if OS X, currentPlatform = "darwin"
+import sys
+currentPlatform = sys.platform
+
+
+
+#OS X only: import Objective C library for mouse control
+
+from Quartz.CoreGraphics import CGEventCreateMouseEvent
+from Quartz.CoreGraphics import CGEventPost
+from Quartz.CoreGraphics import kCGEventMouseMoved
+from Quartz.CoreGraphics import kCGEventLeftMouseDown
+from Quartz.CoreGraphics import kCGEventLeftMouseDown
+from Quartz.CoreGraphics import kCGEventLeftMouseUp
+from Quartz.CoreGraphics import kCGMouseButtonLeft
+from Quartz.CoreGraphics import kCGHIDEventTap
+
+from Quartz import CGDisplayBounds
+from Quartz import CGMainDisplayID
+
+def mouseEvent(type, posx, posy):
+        theEvent = CGEventCreateMouseEvent(
+                    None,
+                    type,
+                    (posx,posy),
+                    kCGMouseButtonLeft)
+        CGEventPost(kCGHIDEventTap, theEvent)
+
+def mousemove(posx,posy):
+        mouseEvent(kCGEventMouseMoved, posx,posy);
+
+def mouseclick(posx,posy):
+        # uncomment this line if you want to force the mouse
+        # to MOVE to the click location first (I found it was not necessary).
+        #mouseEvent(kCGEventMouseMoved, posx,posy);
+        mouseEvent(kCGEventLeftMouseDown, posx,posy);
+        mouseEvent(kCGEventLeftMouseUp, posx,posy);
+
+def screenSize():
+    mainMonitor = CGDisplayBounds(CGMainDisplayID())
+    return (mainMonitor.size.width, mainMonitor.size.height)
+
+
+
+
+
+
+#Connected Devices
 devices = {};
 
 
@@ -28,7 +79,17 @@ class mouseletProtocol(Protocol):
 def processMessage(message, fn):
     splitmsg = message.strip().split(":");
     if splitmsg[0] == "verify":
+        devices[splitmsg[1]] = {}
+        devices[splitmsg[1]]["x"] = screenSize()[0]/2
+        devices[splitmsg[1]]["y"] = screenSize()[1]/2
         fn("sendstart:"+splitmsg[1])
+    elif splitmsg[0] == "data":
+        devices[splitmsg[1]]["x"] = devices[splitmsg[1]]["x"] + float(splitmsg[2])
+        newX = devices[splitmsg[1]]["x"]
+        devices[splitmsg[1]]["y"] = devices[splitmsg[1]]["y"] + float(splitmsg[3])
+        newY = devices[splitmsg[1]]["y"]
+        mousemove(newX, newY)
+
 
 def main():
     factory = Factory()

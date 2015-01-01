54b78fc52fdd294d28d6651006045df3ed1656b0
kyewei <kyewei22@yahoo.ca>
2014-12-31 21:21:12 -0500

    Added A LOT of comments, and error checking. Now segmented messages won't be discarded.

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ currentPlatform = sys.platform
 
 #OS X only: import Objective C library for mouse control
 if currentPlatform == "darwin":
+    #First import all things from library
     from Quartz.CoreGraphics import CGEventCreateMouseEvent
     from Quartz.CoreGraphics import CGEventPost
     from Quartz.CoreGraphics import CGEventSetIntegerValueField
@@ -31,11 +32,12 @@ if currentPlatform == "darwin":
     from Quartz.CoreGraphics import kCGMouseButtonRight
     from Quartz.CoreGraphics import kCGHIDEventTap
 
-    from Quartz import CGDisplayBounds
+    from Quartz import CGDisplayBounds #Also import monitor size
     from Quartz import CGMainDisplayID
 
     #global mousemove, mouseclick, mousedown, mouseup, screenSize
     def mouseEvent(type,posx,posy):
+        #Make Quartz Event and Post (instantiate) it
         theEvent = CGEventCreateMouseEvent(
                     None,
                     type,
@@ -44,11 +46,14 @@ if currentPlatform == "darwin":
         CGEventPost(kCGHIDEventTap, theEvent)
 
     def mousemove(posx,posy,isHeldL,isHeldR,device):
+        #Move mouse to x,y
         mouseEvent(kCGEventMouseMoved,posx,posy)
+        #Only OS X has this quirk where dragging must also be explicitly done, 
+        #  instead of just doing mouseDown + move
         if isHeldL:
             mouseEvent(kCGEventLeftMouseDragged,posx,posy)
         if isHeldR:
-                mouseEvent(kCGEventRightMouseDragged,posx,posy)
+            mouseEvent(kCGEventRightMouseDragged,posx,posy)
 
     def mouseclick(posx,posy,device,count):
         theEvent = CGEventCreateMouseEvent(
@@ -56,7 +61,8 @@ if currentPlatform == "darwin":
                     kCGEventLeftMouseDown,
                     (posx,posy),
                     kCGMouseButtonLeft)
-
+        #Had to manually do the clicking, since kCGMouseEventClickState needs to be set as well for double click to register
+        #  Simply doing up+down x2 was not enough
         CGEventSetIntegerValueField(theEvent,kCGMouseEventClickState,count)
         CGEventPost(kCGHIDEventTap, theEvent)
         CGEventSetType(theEvent,kCGEventLeftMouseUp)
@@ -68,7 +74,7 @@ if currentPlatform == "darwin":
             CGEventSetType(theEvent,kCGEventLeftMouseUp)
             CGEventPost(kCGHIDEventTap, theEvent)
 
-    def mousedown(posx,posy,device,isLeft):
+    def mousedown(posx,posy,device,isLeft): 
         if isLeft:
             mouseEvent(kCGEventLeftMouseDown, posx,posy)
         else:
@@ -102,10 +108,10 @@ elif currentPlatform == "win32":
 
     mouse_event = ctypes.windll.user32.mouse_event
     #SetCursorPos = ctypes.windll.user32.SetCursorPos
-    def getX(posx,maxx):
+    def getX(posx,maxx): #Needed to convert to appropriate numbers for who knows why
         return 65536L * long(posx) / maxx + 1
 
-    def getY(posy,maxy):
+    def getY(posy,maxy): #Needed to convert to appropriate numbers for who knows why
         return 65536L * long(posy) / maxy + 1
 
     def screenSize():
@@ -119,7 +125,7 @@ elif currentPlatform == "win32":
         mouse_event(MOUSEEVENTF_MOVEABS,getX(posx,device["xM"]),getY(posy,device["yM"]),0,0)
         #SetCursorPos(posx,posy)
 
-    def mouseclick(posx,posy,device,count):
+    def mouseclick(posx,posy,device,count): #Windows API is simpler, just down+up x number_of_times
         for i in range(count):
             mousedown(posx,posy,device,True)
             mouseup(posx,posy,device,True)
@@ -153,24 +159,25 @@ elif currentPlatform == "linux2":
     def mousemove(posx,posy,isHeldL,isHeldR,device):
         fake_input(display, X.MotionNotify, x=posx, y=posy)
         display.sync()
-
+        
+    #LMB is X.Button1, RMB is X.Button3
     def mousedown(posx,posy,device,isLeft):
         mousemove(posx,posy,False,False,device)
         if isLeft:
-            fake_input(display, X.ButtonPress, 1)
+            fake_input(display, X.ButtonPress, X.Button1)
         else:
-            fake_input(display, X.ButtonPress, 3)
+            fake_input(display, X.ButtonPress, X.Button3)
         display.sync()
 
     def mouseup(posx,posy,device,isLeft):
         mousemove(posx,posy,False,False,device)
         if isLeft:
-            fake_input(display, X.ButtonRelease, 1)
+            fake_input(display, X.ButtonRelease, X.Button1)
         else:
-            fake_input(display, X.ButtonRelease, 3)
+            fake_input(display, X.ButtonRelease, X.Button3)
         display.sync()
 
-    def mouseclick(posx,posy,device,count):
+    def mouseclick(posx,posy,device,count): #Simple down+up x count times
         for i in range(count):
             mousedown(posx,posy,device,True)
             mouseup(posx,posy,device,True)
@@ -179,26 +186,31 @@ elif currentPlatform == "linux2":
 devices = {};
 
 
-def processMessage(message, fn):
+def processMessage(message, fn): #Functions returns None if no problems, or incomplete last line if line is split up
     # if theres a delay, messages are sent in bursts, so separate out each line
     splitmsgarr =message.strip().split("\n")
     for line in splitmsgarr: #then do processing on each line, which is a command
         splitmsg = line.strip().split(":")
         
-        if splitmsg[0] == "verify" and len(splitmsg) == 2:
+        #Processing of various commands. Look in protocolMessageSpec.txt for more information
+        
+        if len(splitmsg) == 2 and splitmsg[0] == "verify": #First message from client to server
             screenS = screenSize()
-            devices[splitmsg[1]] = {}
-            devices[splitmsg[1]]["x"] = screenS[0]/2
-            devices[splitmsg[1]]["y"] = screenS[1]/2
+            devices[splitmsg[1]] = {} #Resets fields
+            devices[splitmsg[1]]["x"] = screenS[0]/2 * 1.0
+            devices[splitmsg[1]]["y"] = screenS[1]/2 * 1.0
             devices[splitmsg[1]]["xM"] = screenS[0]
             devices[splitmsg[1]]["yM"] = screenS[1]
             devices[splitmsg[1]]["LMBHeld"] = False
             devices[splitmsg[1]]["RMBHeld"] = False
-            fn("sendstart:"+splitmsg[1])
-        elif splitmsg[1] and devices[splitmsg[1]]:
+            fn("sendstart:"+splitmsg[1]) #Server reply for client to start sending
+        
+        #Check to make sure splitmsg[1] and devices lookup exist to get rid of error
+        elif len(splitmsg) >=2 and splitmsg[1] in devices: 
             mouse = devices[splitmsg[1]]
-
-            if splitmsg[0] == "data" and len(splitmsg) == 5:
+            
+            #for each, check to see if Message is complete (has the required length)
+            if len(splitmsg) == 5 and splitmsg[0] == "data": #Mouse position change
                 mouse["x"] = mouse["x"] + float(splitmsg[2])
                 newX = mouse["x"]
                 mouse["y"] = mouse["y"] + float(splitmsg[3])
@@ -210,13 +222,13 @@ def processMessage(message, fn):
                     fn("resetY:"+splitmsg[1])
                     newY = max(min(newY,mouse["yM"]-1-1),0+1)
                 mousemove(newX,newY,mouse["LMBHeld"],mouse["RMBHeld"],mouse)
-            elif splitmsg[0] == "reset" and len(splitmsg) == 2:
+            elif len(splitmsg) == 2 and splitmsg[0] == "reset": #Reset mouse position to center
                 mouse["x"] = mouse["xM"]/2
                 mouse["y"] = mouse["yM"]/2
                 newX = mouse["x"]
                 newY = mouse["y"]
                 mousemove(newX,newY,mouse["LMBHeld"],mouse["RMBHeld"],mouse)
-            elif splitmsg[0] == "statusLMB" and len(splitmsg) == 3:
+            elif len(splitmsg) == 3 and splitmsg[0] == "statusLMB": #Tells server status of LMB: held/not held
                 wasHeld = mouse["LMBHeld"]
                 isHeld = splitmsg[2] == "1"
                 if (wasHeld != isHeld):
@@ -225,7 +237,7 @@ def processMessage(message, fn):
                         mousedown(mouse["x"],mouse["y"],mouse,True)
                     else:
                         mouseup(mouse["x"],mouse["y"],mouse,True)
-            elif splitmsg[0] == "statusRMB" and len(splitmsg) == 3:
+            elif len(splitmsg) == 3 and splitmsg[0] == "statusRMB": #Tells server status of RMB: held/not held
                 wasHeld = mouse["RMBHeld"]
                 isHeld = splitmsg[2] == "1"
                 if (wasHeld != isHeld):
@@ -234,11 +246,14 @@ def processMessage(message, fn):
                         mousedown(mouse["x"],mouse["y"],mouse,False)
                     else:
                         mouseup(mouse["x"],mouse["y"],mouse,False)
-            elif splitmsg[0] == "LMBDoubleClick" and len(splitmsg) == 2:
+            elif len(splitmsg) == 2 and splitmsg[0] == "LMBDoubleClick": #Tells server to execute doubleclick
                 mouseclick(mouse["x"],mouse["y"],mouse,2)
+            else: #Assume message is incomplete, return incomplete line
+                return line
+        else: #Assume message is incomplete, return incomplete line
+            return line
 
-
-def wrapfn(sendfn):
+def wrapfn(sendfn): #Wrap and curry so every message is logged
     def displaysent(message):
         print "Server sent messsage:",message
         sendfn(message+"\n")
@@ -247,7 +262,7 @@ def wrapfn(sendfn):
 def main():
     global socket #apparently imported things don't show up
     HOST = '' #all interfaces
-    PORT = 22096
+    PORT = 22096 #Hmm. These numbers aren't just random.
     CONNECTION_LIST = []
     RECV_BUFFER = 1024
 
@@ -258,7 +273,13 @@ def main():
     CONNECTION_LIST.append(server_socket)
     print "mouselet communication server ("+str(hex(id(server_socket)))+") started on port:", PORT
     print "Total",len(CONNECTION_LIST),"connections(s)", map(lambda x:hex(id(x)),CONNECTION_LIST)
-
+    
+    #Associative array to lookup incomplete lines
+    #Sometimes when a connection is really bad, lines are broken up mid-transfer
+    #This saves the incomplete last line of a message, which will be prepended to the data of the next successful transfer
+    incompleteMessage = {}
+    
+    
     while True:
         read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST, [], [])
 
@@ -267,13 +288,22 @@ def main():
                 client_socket, address = server_socket.accept()
                 CONNECTION_LIST.append(client_socket)
                 print "A client ("+str(hex(id(client_socket)))+") connected, total",len(CONNECTION_LIST),"connections(s):", map(lambda x:hex(id(x)),CONNECTION_LIST)
+                incompleteMessage[str(hex(id(client_socket)))] = ""
 
             else:
                 #try:
                     data = socket.recv(RECV_BUFFER)
                     if data:
                         print "Data received from "+str(hex(id(socket)))+":",data
-                        processMessage(data,wrapfn(socket.sendall))
+                        
+                        #Append previously incomplete last line
+                        appendedData = incompleteMessage[str(hex(id(socket)))] + data 
+                        incompleteMessage[str(hex(id(socket)))] = ""
+                        
+                        #Process message
+                        result = processMessage(appendedData,wrapfn(socket.sendall))
+                        if result != None: #function returned incomplete last line
+                            incompleteMessage[str(hex(id(socket)))] = result
                     else:
                         socket.close()
                         CONNECTION_LIST.remove(socket)
